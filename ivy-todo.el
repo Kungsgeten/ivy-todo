cfb1b9a0da599170344a452b782474924224b15e
Erik Sjöstrand <sjostrand.erik@gmail.com>
2020-03-22 23:04:53 +0100

    Mostly refactoring and cleanup.

--- ivy-todo.el
+++ ivy-todo.el
@@ -4,7 +4,7 @@
 ;; MIT License
 
 ;; Author: Erik Sjöstrand <sjostrand.erik@gmail.com>
-;; URL: http://github.com/Kungsgeten/ivy-todo
+;; URL: https://github.com/Kungsgeten/ivy-todo
 ;; Version: 1.00
 ;; Keywords: convenience
 ;; Package-Requires: ((ivy "0.8.0") (emacs "25"))
@@ -24,10 +24,9 @@
   "The `org-mode' file upon which ivy-todo operates.
 Level 1 headlines are recognized as TODO lists.")
 
-(defvar ivy-todo-headline nil
+(defvar-local ivy-todo-headline nil
   "The level 1 headline in `ivy-todo-file` which contain the active TODO-list.
 Normally use `ivy-todo--buffer-headline-name' instead of accessing this variable.")
-(make-variable-buffer-local 'ivy-todo-headline)
 
 (defvar ivy-todo-guess-list t
   "Whether to guess TODO list based on the current project.")
@@ -40,29 +39,27 @@ If nil, no tags will be applied.")
   "Return an list of level 1 headlines in `ivy-todo-file'."
   (org-element-map (ivy-todo--ast) 'headline
     (lambda (h1)
-      (when (= (org-element-property :level h1) 1)
-        (org-element-property :raw-value h1)))))
+      (and (= (org-element-property :level h1) 1)
+           (org-element-property :raw-value h1)))))
 
 (defun ivy-todo--get-headline (headline)
-  "Return HEADLINE if HEADLINE exists in `ivy-todo-file'.
-Ask if the user want to add it if it doesn't exist."
+  "Return HEADLINE and its position if HEADLINE exists in `ivy-todo-file'.
+If it doesn't exist, create it."
   (when headline
     (or
      (org-element-map (ivy-todo--ast) 'headline
        (lambda (h1)
-         (when (and (= (org-element-property :level h1) 1)
-                    (equal (org-element-property :raw-value h1) headline))
-           (cons headline (org-element-property :begin h1))))
+         (and (= (org-element-property :level h1) 1)
+              (equal (org-element-property :raw-value h1) headline)
+              (cons headline (org-element-property :begin h1))))
        nil t)
-     (if (y-or-n-p (concat headline " doesn't exist. Would you like to create it?"))
-         (progn
-           (ivy-todo--replace-ast
-            (org-element-adopt-elements
-                (ivy-todo--ast)
-              (org-element-create 'headline `(:level 1 ,:title (,headline)
-                                              :tags ,ivy-todo-default-tags))))
-           (ivy-todo--get-headline headline))
-       nil))))
+     (progn
+       (ivy-todo--replace-ast
+        (org-element-adopt-elements
+            (ivy-todo--ast)
+          (org-element-create 'headline `(:level 1 ,:title (,headline)
+                                          :tags ,ivy-todo-default-tags))))
+       (ivy-todo--get-headline headline)))))
 
 (defun ivy-todo--guess-headline-name ()
   "Guess the headline name associated with the current buffer.
@@ -70,26 +67,24 @@ Return nil if no headline is found, else a string."
   (cond ((and (require 'projectile nil t)
               (projectile-project-name)
               (not (equal (projectile-project-name) "-")))
-         (ivy-todo--get-headline (projectile-project-name)))
+         (projectile-project-name))
         ((require 'find-file-in-project nil t)
-         (ivy-todo--get-headline (file-name-base (directory-file-name (ffip-get-project-root-directory)))))
+         (file-name-base (directory-file-name (ffip-get-project-root-directory))))
         ((and (version<= "25" emacs-version)
               (require 'vc)
               (vc-root-dir))
-         (ivy-todo--get-headline (file-name-base (directory-file-name (vc-root-dir)))))
+         (file-name-base (directory-file-name (vc-root-dir))))
         (t nil)))
 
 (defun ivy-todo--buffer-headline-name ()
   "Get the name of the headline associated with the current buffer.
 Set `ivy-todo-headline` to the headline name."
-  (if ivy-todo-headline
-      ivy-todo-headline
-    (setq ivy-todo-headline
-          (let ((headlines (ivy-todo--headlines)))
-            (if ivy-todo-guess-list
-                (or (ivy-todo--guess-headline-name)
-                    (ivy-todo--get-headline (completing-read "TODO list: " headlines)))
-              (ivy-todo--get-headline (completing-read "TODO list: " headlines)))))))
+  (or ivy-todo-headline
+      (setq ivy-todo-headline
+            (ivy-todo--get-headline
+             (completing-read "TODO list: " (ivy-todo--headlines)
+                              nil nil (if ivy-todo-guess-list
+                                          (ivy-todo--guess-headline-name)))))))
 
 (defun ivy-todo--list-items ()
   "Return alist of todo items of `ivy-todo-headline'.
@@ -116,20 +111,8 @@ The car is the name and the cdr is the position in `ivy-todo-file'."
 
 (defun ivy-todo--replace-ast (ast)
   "Replace AST of `ivy-todo-file' and save the file."
-  (with-temp-buffer
-    (insert (org-element-interpret-data ast))
-    (write-file ivy-todo-file)))
-
-(defun ivy-todo--old-or-new-item (item pos)
-  "If ITEM exist in `ivy-todo-file' goto its position, else insert it after POS.
-Meant to be used with `ivy-todo-file' as `current-buffer'."
-  (if (stringp item)
-      (progn
-        (goto-char pos)
-        (goto-char (line-end-position))
-        (org-insert-todo-subheading 1)
-        (insert item))
-    (goto-char (cdr item))))
+  (with-temp-file ivy-todo-file
+    (insert (org-element-interpret-data ast))))
 
 ;;;###autoload
 (defun ivy-todo (&optional arg)
@@ -163,6 +146,19 @@ With a `\\[universal-argument] \\[universal-argument]' ARG, change `ivy-todo-fil
              (goto-char (cdr x))
              (org-todo))
            (save-buffer)))))))
+
+;;; Ivy Actions
+
+(defun ivy-todo--old-or-new-item (item pos)
+  "If ITEM exist in `ivy-todo-file' goto its position, else insert it after POS.
+Meant to be used with `ivy-todo-file' as `current-buffer'."
+  (if (stringp item)
+      (progn
+        (goto-char pos)
+        (goto-char (line-end-position))
+        (org-insert-todo-subheading 1)
+        (insert item))
+    (goto-char (cdr item))))
 
 (defun ivy-todo-archive (headline)
   "Goto HEADLINE in `ivy-todo-file' and archive it.
@@ -203,6 +199,7 @@ HEADLINE is a string or a cons (\"headline\" . buffer-pos)."
         (ivy-todo--old-or-new-item headline headline-pos)
         (org-set-tags-command)
         (save-buffer)))))
+
 (defun ivy-todo-set-effort (headline)
   "Goto HEADLINE in `ivy-todo-file' and call `org-set-effort'.
 HEADLINE is a string or a cons (\"headline\" . buffer-pos)."
